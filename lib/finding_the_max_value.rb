39e99264c179fc91c869334ecf20fbf41dbea7c2
Henry Velasquez <hvelas93@gmail.com>
2021-02-10 20:28:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,5 @@
 array=[1,2,3,4,5,4,3,2,1]
-require 'pry'
+#require 'pry'
 def find_max_value(array)
   count=0
   new_value=array[0]
@@ -7,7 +7,7 @@ def find_max_value(array)
   for i in 1..array.length do
     if array[count] > new_value
       new_value=array[count]
-      binding.pry
+  #    binding.pry
     end
     count+=1
   end
